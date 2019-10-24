092d6b6875556e318a637b335f98d05fc2b42baf
Nick Lee <lenchol@amazon.com>
2019-10-24 15:06:00 +0100

    add oldest execution loop

--- lambdas/src/tasks/oldest_execution.py
+++ lambdas/src/tasks/oldest_execution.py
@@ -4,9 +4,11 @@ Task to check if the current execution is the oldest
 import os
 from collections import deque
 
-from boto_factory import get_client
+import boto3
 
-client = get_client("stepfunctions")
+from decorators import with_logger
+
+sf_client = boto3.client("stepfunctions")
 
 
 def paginate(client, method, **kwargs):
@@ -16,12 +18,14 @@ def paginate(client, method, **kwargs):
             yield result
 
 
+@with_logger
 def handler(event, context):
-    executions = paginate(client, client.list_exectuions, **{
+    executions = paginate(sf_client, sf_client.list_executions, **{
         "stateMachineArn": os.getenv("StateMachineArn"),
         "statusFilter": "RUNNING"
     })
     dd = deque(executions, maxlen=1)
     oldest = dd.pop()
+    context.logger.info("Oldest execution: {}".format(oldest["executionArn"]))
 
-    return event["ExecutionId"] == oldest
+    return event["ExecutionId"] == oldest["executionArn"]
