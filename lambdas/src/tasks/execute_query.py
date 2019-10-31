4d976a2813ea3aa472b1bfd2ab4c6063719fb0d2
Nick Lee <lenchol@amazon.com>
2019-10-31 10:54:01 +0000

    building query through code rather than passed in

--- lambdas/src/tasks/execute_query.py
+++ lambdas/src/tasks/execute_query.py
@@ -8,10 +8,39 @@ client = boto3.client("athena")
 @with_logger
 def handler(event, context):
     response = client.start_query_execution(
-        QueryString=event["Query"],
+        QueryString=make_query(event["QueryData"]),
         ResultConfiguration={
             'OutputLocation': 's3://{bucket}/{prefix}/'.format(bucket=event["Bucket"], prefix=event["Prefix"])
         },
     )
 
     return response["QueryExecutionId"]
+
+
+def make_query(query_data):
+    """
+    Returns a query which will look like
+    SELECT $path
+    FROM "db"."table"
+    WHERE col1 in (matchid1, matchid2) OR col1 in (matchid1, matchid2) AND partition_key = value"
+    """
+    template = '''
+    SELECT DISTINCT "$path"
+    FROM "{db}"."{table}"
+    WHERE
+        ({column_filters})
+    '''
+    db = query_data["Database"]
+    table = query_data["Table"]
+    columns = query_data["Columns"]
+    partition = query_data.get("Partition")
+
+    column_filters = ""
+    for i, col in enumerate(columns):
+        if i > 0:
+            column_filters = column_filters + " OR "
+        column_filters = column_filters + "{} in ({})".format(
+            col["Column"], ', '.join("'{0}'".format(u) for u in col["Users"]))
+    if partition:
+        template = template + " AND {key} = '{value}' ".format(key=partition["Key"], value=partition["Value"])
+    return template.format(db=db, table=table, column_filters=column_filters)
--- tests/unit/test_execute_query.py
+++ tests/unit/test_execute_query.py
@@ -1,21 +1,66 @@
+import re
 from types import SimpleNamespace
 
 import pytest
 from mock import patch
 
-from lambdas.src.tasks.execute_query import handler
+from lambdas.src.tasks.execute_query import handler, make_query
 
 pytestmark = [pytest.mark.unit, pytest.mark.task]
 
 
 @patch("lambdas.src.tasks.execute_query.client")
-def test_it_executes_queries(client_mock):
+@patch("lambdas.src.tasks.execute_query.make_query")
+def test_it_executes_queries(query_mock, client_mock):
     client_mock.start_query_execution.return_value = {
         "QueryExecutionId": "123"
     }
+    query_mock.return_value = "test"
 
-    resp = handler({"Query": "blah", "Bucket": "mybucket", "Prefix": "my_prefix"}, SimpleNamespace())
+    resp = handler({"QueryData": {}, "Bucket": "mybucket", "Prefix": "my_prefix"}, SimpleNamespace())
     assert "123" == resp
-    client_mock.start_query_execution.assert_called_with(QueryString="blah", ResultConfiguration={
+    client_mock.start_query_execution.assert_called_with(QueryString="test", ResultConfiguration={
         'OutputLocation': 's3://mybucket/my_prefix/'
     })
+
+
+def test_it_generates_query_with_partition():
+    resp = make_query({
+        "Database": "amazonreviews",
+        "Table": "amazon_reviews_parquet",
+        "Columns": [{"Column": "customer_id", "Users": ["123456", "456789"]}],
+        "Partition": {"Key": "product_category", "Value": "Books"}
+    })
+
+    assert "SELECT DISTINCT \"$path\" " \
+           "FROM \"amazonreviews\".\"amazon_reviews_parquet\" " \
+           "WHERE (customer_id in ('123456', '456789')) " \
+           "AND product_category = 'Books'" == re.sub("[\x00-\x20]+", " ", resp.strip())
+
+
+def test_it_generates_query_without_partition():
+    resp = make_query({
+        "Database": "amazonreviews",
+        "Table": "amazon_reviews_parquet",
+        "Columns": [{"Column": "customer_id", "Users": ["123456", "456789"]}]
+    })
+
+    assert "SELECT DISTINCT \"$path\" " \
+           "FROM \"amazonreviews\".\"amazon_reviews_parquet\" " \
+           "WHERE (customer_id in ('123456', '456789'))" == re.sub("[\x00-\x20]+", " ", resp.strip())
+
+
+def test_it_generates_query_with_multiple_columns():
+    resp = make_query({
+        "Database": "amazonreviews",
+        "Table": "amazon_reviews_parquet",
+        "Columns": [
+            {"Column": "a", "Users": ["a123456", "b123456"]},
+            {"Column": "b", "Users": ["a456789", "b456789"]},
+        ]
+    })
+
+    assert "SELECT DISTINCT \"$path\" " \
+           "FROM \"amazonreviews\".\"amazon_reviews_parquet\" " \
+           "WHERE (a in ('a123456', 'b123456') OR b in ('a456789', 'b456789'))" == re.sub("[\x00-\x20]+", " ",
+                                                                                        resp.strip())
