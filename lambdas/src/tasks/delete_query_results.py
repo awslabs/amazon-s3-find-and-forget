8340521623df7ff558afb1f9555b1d5e25d406ab
Nick Lee <lenchol@amazon.com>
2019-11-07 17:52:52 +0000

    fix tests and key ref

--- lambdas/src/tasks/delete_query_results.py
+++ lambdas/src/tasks/delete_query_results.py
@@ -14,7 +14,7 @@ bucket = s3.Bucket(os.getenv("BucketName"))
 @with_logger
 def handler(event, context):
     # Assume results will be stored under the Execution ID
-    responses = bucket.objects.filter(Prefix="{}/".format(event["ExecutionId"])).delete()
+    responses = bucket.objects.filter(Prefix="{}/".format(event["ExecutionName"])).delete()
     deleted = [item for response in responses for item in response.get("Deleted", [])]
     errors = [item for response in responses for item in response.get("Errors", [])]
     if len(errors) > 0:
