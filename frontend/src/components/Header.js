b855abcac32e247e20ae3ef310b43824e15909f6
Matteo Figus <matteofigus@gmail.com>
2019-12-17 12:22:43 +0000

    Cleanup header, retries on auth info

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,34 +2,55 @@ import { Auth } from "aws-amplify";
 import React, { useEffect, useState } from "react";
 import { Button, Navbar } from "react-bootstrap";
 
+import { retryWrapper } from "../utils";
+import "./Header.css";
+
 export default ({ signedIn }) => {
+  const [authError, setAuthError] = useState(null);
   const [userEmail, setUserEmail] = useState(undefined);
 
-  const signOut = () => {
-    Auth.signOut().then(() => window.location.reload());
-  };
+  const reload = () => window.location.reload();
+
+  const signOut = () =>
+    Auth.signOut()
+      .then(reload)
+      .catch(reload);
 
   useEffect(() => {
     if (signedIn) {
-      Auth.currentAuthenticatedUser()
+      retryWrapper(() => Auth.currentAuthenticatedUser())
         .then(user => setUserEmail(user.username))
-        .catch(() => {});
+        .catch(setAuthError);
     }
   }, [signedIn]);
 
   return (
-    <Navbar style={{ backgroundColor: "#232f3e" }} variant="dark">
+    <Navbar variant="dark">
       <Navbar.Brand>
         <div className="awslogo" />
       </Navbar.Brand>
       <Navbar.Toggle />
-      {userEmail && (
+      {(userEmail || authError) && (
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Welcome {userEmail}
-            <Button variant="link" className="headerLink" onClick={signOut}>
-              Sign Out
-            </Button>
+            {authError && (
+              <>
+                <span className="auth-error">
+                  Authentication error: {authError}
+                </span>
+                <Button variant="link" className="headerLink" onClick={reload}>
+                  Retry
+                </Button>
+              </>
+            )}
+            {userEmail && (
+              <>
+                Welcome {userEmail}
+                <Button variant="link" className="headerLink" onClick={signOut}>
+                  Sign Out
+                </Button>
+              </>
+            )}
           </Navbar.Text>
         </Navbar.Collapse>
       )}
