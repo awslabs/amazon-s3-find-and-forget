31d79ff33306a02ee3960eae906ef81beb91ba09
Matteo Figus <matteofigus@gmail.com>
2019-12-30 18:26:50 +0000

    Cleanup

--- frontend/src/components/pages/Configuration.js
+++ frontend/src/components/pages/Configuration.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Col, Form, Modal, Row, Spinner, Table } from "react-bootstrap";
 
 import Alert from "../Alert";
+import BucketPolicyModal from "../BucketPolicyModal";
 import Icon from "../Icon";
 
 import { formatErrorMessage, sortBy } from "../../utils";
@@ -122,73 +123,10 @@ export default ({ gateway, onPageChange }) => {
           </Modal.Footer>
         </Modal>
       )}
-      <Modal
-        centered
+      <BucketPolicyModal
         show={showingBucketPolicy}
-        size="lg"
-        onHide={() => showBucketPolicy(false)}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>S3 Bucket Policy</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            After configuring the data mappers, you need to configure the
-            relevant S3 bucket policies to enable the solution to write and read
-            from/to them.
-          </p>
-          <p>
-            1. Retrieve the Athena Query Executor (<b>AthenaExecutionRoleArn</b>
-            ) and Delete Task (<b>DeleteTaskIAMRoleArn</b>) IAM roles from the
-            Solution CloudFormation Stack Output.
-          </p>
-          <p>
-            2. Find the relevant Bucket in the S3 AWS Web Console and{" "}
-            <a href="https://s3.console.aws.amazon.com/s3/home" target="_new">
-              click its name
-            </a>
-            , then "Permissions", and finally "Bucket Policy".
-          </p>
-          <p> 3. Edit the Policy and then click "Save". Here is an example:</p>
-          <code>
-            <pre>
-              {JSON.stringify(
-                {
-                  Version: "2012-10-17",
-                  Statement: [
-                    {
-                      Sid: "AllowS3F2",
-                      Effect: "Allow",
-                      Principal: {
-                        AWS: [
-                          "<AthenaExecutionRoleArn>",
-                          "<DeleteTaskIAMRoleArn>"
-                        ]
-                      },
-                      Action: "s3:*",
-                      Resource: [
-                        "arn:aws:s3:::<YOUR_S3_BUCKET_NAME>",
-                        "arn:aws:s3:::<YOUR_S3_BUCKET_NAME>/*"
-                      ]
-                    }
-                  ]
-                },
-                null,
-                2
-              )}
-            </pre>
-          </code>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            className="aws-button action-button"
-            onClick={() => showBucketPolicy(false)}
-          >
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
+        close={() => showBucketPolicy(false)}
+      />
       <Form>
         <Table>
           <thead>
