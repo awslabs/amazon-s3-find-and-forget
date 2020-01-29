9556ce1d069faac50d7137a48c57ab348d121cd4
Nick Lee <lenchol@amazon.com>
2020-01-29 15:22:17 +0000

    wait for task shutdown

---
+++ backend/lambdas/tasks/check_task_count.py
@@ -0,0 +1,29 @@
+"""
+Task to check the number of running and pending tasks
+"""
+import boto3
+
+from decorators import with_logger
+
+client = boto3.client("ecs")
+
+
+@with_logger
+def handler(event, context):
+    try:
+        service = client.describe_services(
+            cluster=event["Cluster"],
+            services=[
+                event["ServiceName"],
+            ],
+        )["services"][0]
+        pending = service["pendingCount"]
+        running = service["runningCount"]
+        return {
+            "Pending": pending,
+            "Running": running,
+            "Total": pending + running
+        }
+    except IndexError:
+        context.logger.error("Unable to find service {}".format(event["ServiceName"]))
+        raise ValueError("Service {} in cluster {} not found".format(event["ServiceName"], event["Cluster"]))
