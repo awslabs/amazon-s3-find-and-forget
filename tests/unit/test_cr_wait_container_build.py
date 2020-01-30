e7cd2dc82591c08a444e7e8219000319e6a6f508
Matteo Figus <matteofigus@gmail.com>
2020-01-30 16:46:28 +0000

    Fix test

--- tests/unit/test_cr_wait_container_build.py
+++ tests/unit/test_cr_wait_container_build.py
@@ -1,8 +1,7 @@
-from types import SimpleNamespace
-import json
-import pytest
-from mock import call, patch, MagicMock, Mock
 from datetime import datetime, timezone
+import boto3
+import pytest
+from mock import patch, MagicMock, Mock
 
 from backend.lambdas.custom_resources.wait_container_build import create, poll, handler
 
@@ -23,13 +22,13 @@ def test_it_signal_readiness_when_image_ready(mock_ecr_client, mock_s3_client):
     mock_ecr_client.describe_images.return_value = {
         'imageDetails': [{
             'imagePushedAt': datetime(2020, 1, 6, 16, 12, 57, tzinfo=timezone.utc)
-        }] 
+        }]
     }
     mock_object = MagicMock()
     mock_s3_client.Object.return_value = mock_object
     mock_object.last_modified = datetime(2020, 1, 6, 16, 8, 51, tzinfo=timezone.utc)
 
-    resp = poll(event, MagicMock())    
+    resp = poll(event, MagicMock())
 
     assert resp
 
@@ -48,13 +47,13 @@ def test_it_keeps_polling_when_image_not_ready(mock_ecr_client, mock_s3_client):
     mock_ecr_client.describe_images.return_value = {
         'imageDetails': [{
             'imagePushedAt': datetime(2020, 1, 6, 14, 0, 13, tzinfo=timezone.utc)
-        }] 
+        }]
     }
     mock_object = MagicMock()
     mock_s3_client.Object.return_value = mock_object
     mock_object.last_modified = datetime(2020, 1, 6, 16, 8, 51, tzinfo=timezone.utc)
 
-    resp = poll(event, MagicMock())    
+    resp = poll(event, MagicMock())
 
     assert not resp
 
@@ -70,12 +69,14 @@ def test_it_keeps_polling_when_no_latest_image_found(mock_ecr_client, mock_s3_cl
         }
     }
 
-    mock_ecr_client.describe_images.side_effect = Mock(side_effect=Exception('No image found for tag latest'))
+    e = boto3.client("ecr").exceptions.ImageNotFoundException
+    mock_ecr_client.exceptions.ImageNotFoundException = e
+    mock_ecr_client.describe_images.side_effect = Mock(side_effect=e({}, 'ImageNotFoundException'))
     mock_object = MagicMock()
     mock_s3_client.Object.return_value = mock_object
     mock_object.last_modified = datetime(2020, 1, 6, 16, 8, 51, tzinfo=timezone.utc)
 
-    resp = poll(event, MagicMock())    
+    resp = poll(event, MagicMock())
 
     assert not resp
 
@@ -86,4 +87,4 @@ def test_it_delegates_to_cr_helper(cr_helper):
     cr_helper.assert_called_with(1, 2)
 
 def test_it_does_nothing_on_create():
-    assert create({}, MagicMock()) == None
+    assert create({}, MagicMock()) is None
