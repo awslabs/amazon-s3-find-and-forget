d6d3a7214a16fb617d410668df8bd4b152327df0
Matteo Figus <matteofigus@gmail.com>
2020-03-26 10:42:04 +0000

    Update backend/lambda_layers/boto_utils/python/boto_utils.py
    Co-Authored-By: Chris Deigan <ctd@users.noreply.github.com>

--- backend/lambda_layers/boto_utils/python/boto_utils.py
+++ backend/lambda_layers/boto_utils/python/boto_utils.py
@@ -212,7 +212,7 @@ def verify_object_versions_integrity(bucket, key, from_version, to_version):
         raise ValueError("version_from ({}) not found".format(from_version))
     if to_index == -1:
         raise ValueError("version_to ({}) not found".format(to_version))
-    if(to_index < from_index):
+    if to_index < from_index:
         raise ValueError("from_version ({}) is more recent than to_version ({})".format(
             from_version,
             to_version
