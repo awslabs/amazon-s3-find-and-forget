725e7176c131a0156e8930c479113d232ff35566
Nick Lee <lenchol@amazon.com>
2020-04-15 19:28:31 +0100

    fix unit and acceptance tests

--- backend/lambdas/jobs/handlers.py
+++ backend/lambdas/jobs/handlers.py
@@ -15,8 +15,13 @@ table = ddb.Table(os.getenv("JobTable", "S3F2_Jobs"))
 index = os.getenv("JobTableDateGSI", "Date-GSI")
 bucket_count = int(os.getenv("GSIBucketCount", 1))
 
-end_events = [
-    "CleanupSucceeded", "CleanupFailed", "CleanupSkipped"
+end_statuses = [
+    "COMPLETED_CLEANUP_FAILED",
+    "COMPLETED",
+    "FAILED",
+    "FIND_FAILED",
+    "FORGET_FAILED",
+    "FORGET_PARTIALLY_FAILED"
 ]
 
 
@@ -98,7 +103,11 @@ def list_job_events_handler(event, context):
             'Id': job_id,
             'Sk': job_id,
         }
-    )
+    ).get("Item")
+    if not job:
+        return {
+            "statusCode": 404
+        }
 
     watermark_boundary_mu = job.get("JobFinishTime", utc_timestamp()) * 1000
 
@@ -118,34 +127,60 @@ def list_job_events_handler(event, context):
     # items after filtering is still page_size+1 i.e. the Job item wasn't on the page.
     items = []
     query_start_key = str(start_at)
+    last_evaluated = None
     while len(items) < page_size:
         resp = table.query(
             KeyConditionExpression=Key('Id').eq(job_id),
             ScanIndexForward=True,
             FilterExpression=filter_expression,
-            Limit=100 if len(user_filters) > 0 else page_size + 1,
+            Limit=100 if len(user_filters) else page_size + 1,
             ExclusiveStartKey={
                 "Id": job_id,
                 "Sk": query_start_key
             }
         )
         results = resp.get("Items", [])
-        items.extend(results[:page_size])
-        if not resp.get("LastEvaluatedKey"):
+        items.extend(results[:page_size - len(items)])
+        query_start_key = resp.get("LastEvaluatedKey", {}).get("Sk")
+        if not query_start_key:
             break
+        last_evaluated = query_start_key
 
-        query_start_key = resp["LastEvaluatedKey"]["Sk"]
+    next_start = _get_watermark(items, start_at, page_size, job["JobStatus"], query_start_key, last_evaluated)
 
-    resp = {
-        "JobEvents": items
-    }
-    if len(items) > 0:
-        if not any([i["EventName"] in end_events for i in items]):
-            resp["NextStart"] = items[len(items) - 1]["Sk"]
-    if len(items) == 0:
-        resp["NextStart"] = start_at
+    resp = {k: v for k, v in {
+        "JobEvents": items,
+        "NextStart": next_start
+    }.items() if v is not None}
 
     return {
         "statusCode": 200,
         "body": json.dumps(resp, cls=DecimalEncoder)
     }
+
+
+def _get_watermark(items, initial_start_key, page_size, job_status, last_query_start_key, last_evaluated_ddb_key):
+    """
+    Work out the watermark to return to the user using the following logic:
+    1. If the job is in progress. In this scenario we always return a watermark but the source of the watermark
+       is determined as follows:
+       a. We've cycled through multiple DDB pages the last available items in DDB but filtering has left us with less
+          than the desired page so return the last evaluated DDB key
+       b. We've either cycled through all the DDB pages and fulfilled the page size OR we're on the first DDB page still
+       c. There's no new events yet so just return whatever the user sent
+    2. If the job is finished, return a watermark if the last page hasn't been reached
+    """
+    next_start = None
+    if job_status not in end_statuses:
+        # Job is in progress
+        if len(items) < page_size and last_evaluated_ddb_key:
+            next_start = last_evaluated_ddb_key
+        elif 0 < len(items) <= page_size:
+            next_start = items[len(items) - 1]["Sk"]
+        else:
+            next_start = str(initial_start_key)
+    # If the job is finished but there are potentially more results
+    elif len(items) == page_size and last_query_start_key:
+        next_start = items[len(items) - 1]["Sk"]
+
+    return next_start
