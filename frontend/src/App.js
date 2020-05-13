df8481e81577a3e10f28f4d43cea0a2d230c3463
Matteo Figus <matteofigus@gmail.com>
2020-05-13 15:13:01 +0100

    Cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Authenticator, Greetings, SignUp } from "aws-amplify-react";
 
 import AppLayout from "./components/AppLayout";
