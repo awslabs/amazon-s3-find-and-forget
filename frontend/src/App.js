85579a68eb8a6e9db8c72802aa27085a3aa935e3
Matteo Figus <matteofigus@gmail.com>
2020-05-18 09:31:00 +0100

    Set autocomplete OFF on sign in form (#152)
    * Set autocomplete OFF on sign in form
    
    * Clarify around the useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Authenticator, Greetings, SignUp } from "aws-amplify-react";
+import React, { useEffect, useState } from "react";
+import { AmplifyAuthenticator, AmplifySignIn } from "@aws-amplify/ui-react";
 
 import AppLayout from "./components/AppLayout";
 import Header from "./components/Header";
@@ -22,6 +22,8 @@ import {
   NewDeletionQueueMatchPage,
 } from "./components/pages";
 import gateway from "./utils/gateway";
+import { isUndefined } from "./utils";
+import { Auth } from "aws-amplify";
 
 export default () => {
   const [authState, setAuthState] = useState(undefined);
@@ -100,12 +102,25 @@ export default () => {
     },
   ];
 
-  const classNames = ["App"];
-  const signedIn = authState === "signedIn";
-  if (!signedIn) classNames.push("amplify-auth");
+  useEffect(() => {
+    if (isUndefined(authState)) {
+      // We want to establish if authState is undefined because the user is not authenticated
+      // or because the state hasn't been updated yet by the authenticator. This is important
+      // because the <AmplifyAuthenticator> doesn't show if the user is already authenticated,
+      // which is a scenario that can happen if the user closes the page and we want to persist
+      // its session.
+      Auth.currentAuthenticatedUser()
+        .then(() => setAuthState("signedin"))
+        .catch(() => {
+          // The user appears unauthenticated. All fine. The <AmplifyAuthenticator> will be
+          // rendered correctly.
+        });
+    }
+  }, [authState]);
 
+  const signedIn = authState === "signedin";
   return (
-    <div className={classNames.join(" ")}>
+    <div className="App">
       <Header signedIn={signedIn} />
       {signedIn ? (
         <AppLayout
@@ -114,10 +129,33 @@ export default () => {
           pages={pages}
         />
       ) : (
-        <Authenticator
-          onStateChange={(s) => setAuthState(s)}
-          hide={[Greetings, SignUp]}
-        />
+        <div className="amplify-auth-container">
+          <AmplifyAuthenticator usernameAlias="email">
+            <AmplifySignIn
+              slot="sign-in"
+              usernameAlias="email"
+              handleAuthStateChange={(s) => setAuthState(s)}
+              formFields={[
+                {
+                  type: "email",
+                  label: "Username *",
+                  placeholder: "Enter your username",
+                  required: true,
+                  inputProps: { autoComplete: "off" },
+                },
+                {
+                  type: "password",
+                  label: "Password *",
+                  placeholder: "Enter your password",
+                  required: true,
+                  inputProps: { autoComplete: "off" },
+                },
+              ]}
+            >
+              <div slot="secondary-footer-content"></div>
+            </AmplifySignIn>
+          </AmplifyAuthenticator>
+        </div>
       )}
     </div>
   );
