88d2360252ce66efd5c4ffb8a4b3ba8aacbbe78b
Matteo Figus <matteofigus@gmail.com>
2020-07-07 18:57:05 +0100

    Generate queries handler and tests

--- backend/lambdas/tasks/generate_queries.py
+++ backend/lambdas/tasks/generate_queries.py
@@ -107,13 +107,128 @@ def get_partitions(db, table_name):
     return paginate(glue_client, glue_client.get_partitions, ["Partitions"], DatabaseName=db, TableName=table_name)
 
 
-def convert_to_col_type(val, col, table):
-    column = next((i for i in table["StorageDescriptor"]["Columns"] if i["Name"] == col), None)
-    if not column:
-        return str(val)
-        # raise ValueError("Column {} not found".format(col))
 
-    col_type = column["Type"]
+arraystruct = "array<struct>"
+arraystruct_prefix = "array<struct<"
+arraystruct_suffix = ">>"
+
+struct = "struct"
+struct_prefix = "struct<"
+struct_suffix = ">"
+
+schema_invalid = "Column schema is not valid"
+allowed_types = [
+    "bigint",
+    "char",
+    "double",
+    "float",
+    "int",
+    "smallint",
+    "string",
+    "tinyint",
+    "varchar"
+  ]
+
+def get_inner_children(str, prefix, suffix):
+    if not str.endswith(suffix):
+        raise ValueError(schema_invalid)
+    return str[len(prefix):(len(str) - len(suffix))]
+
+def get_nested_children(str, nested_type):
+    is_struct = nested_type == struct
+    prefix = struct_prefix if is_struct else arraystruct_prefix
+    suffix = struct_suffix if is_struct else arraystruct_suffix
+    n_opened_tags = len(suffix)
+    end_index = -1
+
+    to_parse = str[len(prefix):]
+    for i in range(len(to_parse)):
+        char = to_parse[i:(i+1)]
+        if char == "<": n_opened_tags +=1
+        if char == ">": n_opened_tags -=1
+        if n_opened_tags == 0:
+            end_index = i
+            break
+
+    if end_index < 0: raise ValueError(schema_invalid)
+    return str[0:(end_index + len(prefix) + 1)]
+
+def get_nested_type(str):
+    upper_index = str.find(",")
+    return str[0:upper_index] if upper_index >= 0 else str
+
+def set_no_identifier_to_node_and_its_children(node):
+    node["CanBeIdentifier"] = False
+    for child in node.get('Children', []):
+        set_no_identifier_to_node_and_its_children(child)
+
+def column_mapper(col):
+    prefix = suffix = None
+    result_type = col['Type']
+    has_children = False
+
+    if result_type.startswith(arraystruct_prefix):
+        result_type = arraystruct
+        prefix = arraystruct_prefix
+        suffix = arraystruct_suffix
+        has_children = True
+    elif result_type.startswith(struct_prefix):
+        result_type = struct
+        prefix = struct_prefix
+        suffix = struct_suffix
+        has_children = True
+    
+    result = {
+        'Name': col['Name'],
+        'Type': result_type,
+        'CanBeIdentifier': col['CanBeIdentifier'] if 'CanBeIdentifier' in col else result_type in allowed_types
+    }
+
+    if has_children:
+        result['Children'] = []
+        children_to_parse = get_inner_children(col['Type'], prefix, suffix)
+        
+        while len(children_to_parse) > 0:
+            sep = ":"
+            name = children_to_parse[0:children_to_parse.index(sep)]
+            rest = children_to_parse[len(name) + len(sep):]
+            nested_type = "other"
+            if rest.startswith(struct_prefix): nested_type = struct
+            elif rest.startswith(arraystruct_prefix): nested_type = arraystruct
+
+            c_type = get_nested_type(rest) if nested_type == "other" else get_nested_children(rest, nested_type)
+            result['Children'].append(column_mapper({
+                'Name': name,
+                'Type': c_type,
+                'CanBeIdentifier': c_type in allowed_types
+            }))
+            children_to_parse = children_to_parse[len(name) + len(sep) + len(c_type):]
+            if children_to_parse.startswith(','): children_to_parse = children_to_parse[1:]
+
+        if result_type != struct: set_no_identifier_to_node_and_its_children(result)
+    
+    return result
+
+
+def find_column_type(col, table):
+    table_columns = table["StorageDescriptor"]["Columns"]
+    col_array = col.split(".")
+    serialized_cols = list(map(column_mapper, table_columns))
+    found = None
+    for col_segment in col_array:
+        found = next((x for x in serialized_cols if x['Name'] == col_segment), None)
+        if not found: return None
+        serialized_cols = found['Children'] if 'Children' in found else []
+    if not found['CanBeIdentifier']:
+        raise ValueError("Column {} is not a supported column type for querying".format(found['Name']))
+
+    return found['Type']
+    
+
+def convert_to_col_type(val, col, table):
+    col_type = find_column_type(col, table)
+    if not col_type:
+        raise ValueError("Column {} not found".format(col))
 
     if col_type in ("char", "string", "varchar"):
         return str(val)
@@ -122,4 +237,4 @@ def convert_to_col_type(val, col, table):
     if col_type in ("double", "float"):
         return float(val)
 
-    raise ValueError("Column {} is type {} which is not a supported column type for querying")
+    raise ValueError("Column {} is not a supported column type for querying".format(col))
